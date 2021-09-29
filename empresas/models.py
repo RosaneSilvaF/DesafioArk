139a854d9006857d36e6a9e09c39ad4936e0acc7
RosaneSilvaF <rosanesfaraujo@gmail.com>
2021-09-29 11:08:46 -0300

    Datables creation - initial

--- empresas/models.py
+++ empresas/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+class Empresa(models.Model):
+    id = models.IntegerField(primary_key=True)
+    tipo = models.IntegerField()
+    nome = models.CharField(max_length=100)
+    nome_fantasia = models.CharField(max_length=100,null=True)
+    superior = models.CharField(max_length=100, null=True)
+    cnpj = models.CharField(max_length=20,null=True)
+    observacoes = models.TextField(null=True)
+    contato = models.CharField(max_length=100, null=True)
+    email = models.EmailField(null=True)
+    telefone2 = models.CharField(max_length=20,null=True)
+    ramal2 = models.CharField(max_length=20,null=True)
+    telefone1 = models.CharField(max_length=20,null=True)
+    ramal1 = models.CharField(max_length=20,null=True)
+    fax = models.CharField(max_length=20,null=True)
+    cep = models.CharField(max_length=20,null=True)
+    rua = models.CharField(max_length=100,null=True)
+    numero = models.CharField(max_length=20,null=True)
+    complemento = models.CharField(max_length=100,null=True)
+    bairro = models.CharField(max_length=100,null=True)
+    cidade = models.CharField(max_length=100,null=True)
+    estado = models.CharField(max_length=100,null=True)
+
+class Equipamento(models.Model):
+    id = models.IntegerField(primary_key=True)
+    fabricante = models.CharField(max_length=100,null=True)
+    modelo = models.CharField(max_length=100,null=True)
+    patrimonio = models.CharField(max_length=100,null=True)
+    numero_serie = models.CharField(max_length=50,null=True)
+    proprietario = models.IntegerField()
+
+class Chamado(models.Model):
+    id = models.IntegerField(primary_key=True)
+    numero_chamado = models.IntegerField()
+    solocitante = models.CharField(max_length=100)
+    equipamento = models.CharField(max_length=100)
+    prioridade = models.IntegerField()
--- empresas/views.py
+++ empresas/views.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from django.shortcuts import render
+from .models import Empresa, Equipamento, Chamado
 import requests
 import string
 import random
@@ -26,7 +27,6 @@ HEADER = {'Authorization': 'JWT '+ login(),'Content-Type': 'application/json'}
 #------------------------------------Métodos dos equipamentos e empresas------------------------------------------------------------------------------#
 
 def get_empresas():
-    token = login()
     response = requests.request("GET",URL + 'empresa/', headers=HEADER, data='').json()
     return response
 
@@ -37,7 +37,6 @@ def get_equipamentos():
     return lista_tipo('equipamentos_paginados/?empresa_id=')
 
 def chamado_equipamento(request):
-    token = login()
     response = []
     texto = gerador_texto()
     equipamentos = get_equipamentos()
@@ -63,25 +62,9 @@ def equipamentos(request):
     response = get_equipamentos()
     return render(request, 'equipamentos.html', {'response':response})
 
-#------------------------------------------------Funções auxiliares---------------------------------------------------------------------#
-
-# Reference : https://www.horadecodar.com.br/2021/04/15/gerar-string-com-letras-e-numeros-aleatorios-em-python/
-
-def gerador_palavra():
-    tamanho = 10
-    chars = string.ascii_letters + string.digits
-    palavra = ''.join(random.choice(chars) for i in range(tamanho)) # palavras aleatórias de 10 letras
-    return palavra + ' '
-
-def gerador_texto():
-    tamanho = 90
-    texto = ''.join(gerador_palavra() for i in range(tamanho))
-    return texto
-
 #------------------------------------Loop principal - lista empresa e equipamentos---------------------------------------------------------------------#
 
 def lista_tipo(sufixo):
-    token = login()
     companies = get_empresas()
     response = []
     i=0
@@ -96,3 +79,74 @@ def lista_tipo(sufixo):
                 response.append(requests.request("GET", URL + sufixo + str(id), headers=HEADER, data='').json())
             
     return response
+
+#---------------------------------------------------Salvar no BD---------------------------------------------------------------------------------------#
+
+def salva_empresas_bd():
+    lista_empresas = detalhes_empresas()
+    for empresa in lista_empresas:
+        Empresa(
+            id = empresa['id'],
+            tipo = empresa['tipo'],
+            nome = empresa['nome'],
+            nome_fantasia = empresa['nome_fantasia'],
+            superior = empresa['superior'],
+            cnpj = empresa['cnpj'],
+            observacoes = empresa['observacoes'],
+            contato = empresa['contato'],
+            email = empresa['email'],
+            telefone2 = empresa['telefone2'],
+            ramal2 = empresa['ramal2'],
+            telefone1 = empresa['telefone1'],
+            ramal1 = empresa['ramal1'],
+            fax = empresa['fax'],
+            cep = empresa['cep'],
+            rua = empresa['rua'],
+            numero = empresa['numero'],
+            complemento = empresa['complemento'],
+            bairro = empresa['bairro'],
+            cidade = empresa['cidade'],
+            estado = empresa['estado']
+        ).save()
+
+
+def salva_equipamentos_bd():
+    lista_equipamentos = get_equipamentos()['results']
+    for equipamento in lista_equipamentos:
+        Equipamento(
+            id = equipamento['id'],
+            fabricante = equipamento['fabricante'],
+            modelo = equipamento['modelo'],
+            patrimonio = equipamento['patrimonio'],
+            numero_serie = equipamento['numero_serie'],
+            proprietario = equipamento['proprietario']['id']
+        ).save()
+
+def salva_chamados_bd():
+    lista_equipamentos = get_equipamentos()['results']
+    chamado = None
+    for equipamento in lista_equipamentos:
+        chamado = requests.request("GET", URL + 'chamado/?equipamento_id=' + equipamento['id'], headers=HEADER, data='').json()
+        print(chamado)
+        Chamado(
+            id = chamado['id'],
+            numero_chamado = chamado['chamados'],
+            solocitante = chamado['get_solicitante'],
+            equipamento = chamado['get_equipamento_servico'],
+            prioridade = chamado['prioridade']
+        ).save()
+
+#------------------------------------------------Funções auxiliares---------------------------------------------------------------------#
+
+# Referencia : https://www.horadecodar.com.br/2021/04/15/gerar-string-com-letras-e-numeros-aleatorios-em-python/
+
+def gerador_palavra():
+    tamanho = 10
+    chars = string.ascii_letters + string.digits
+    palavra = ''.join(random.choice(chars) for i in range(tamanho)) # palavras aleatórias de 10 letras
+    return palavra + ' '
+
+def gerador_texto():
+    tamanho = 90
+    texto = ''.join(gerador_palavra() for i in range(tamanho))
+    return texto
